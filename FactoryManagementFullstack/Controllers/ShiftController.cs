ef59973c59c449ce17a2f0b79f76de9747ebc166
Daniel Kandalaft <kanddany1985@gmail.com>
2021-04-14 14:11:13 +0300

    Completed first Fullstack project in ASP.Net

---
+++ FactoryManagementFullstack/Controllers/ShiftController.cs
@@ -0,0 +1,72 @@
+﻿using FactoryManagementFullstack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FactoryManagementFullstack.Controllers
+{
+    public class ShiftController : Controller
+    {
+        static ShiftBL shiftBL = new ShiftBL();
+        // GET: Shift
+        public ActionResult Index()
+        {
+            if (Session["fullName"] != null)
+            {
+                return View("Show");
+            }
+            else
+            {
+                return Redirect("/LogIn/");
+            }
+        }
+
+        public ActionResult Show()
+        {
+            if (Session["fullName"] != null)
+            {
+                ViewBag.ShiftsList = shiftBL.GetAllEmployeeShifts();
+                EmployeeBL employeeBL = new EmployeeBL();
+                ViewBag.AllShifts = employeeBL.GetAllShifts();
+                return View("Show");
+            }
+            else
+            {
+                return Redirect("/LogIn/");
+            }
+        }
+
+        public ActionResult Add()
+        {
+            if (Session["fullName"] != null)
+            {
+                EmployeeBL employeeBL = new EmployeeBL();
+                ViewBag.AllShifts = employeeBL.GetAllShifts();
+                return View("Add");
+            }
+            else
+            {
+                return Redirect("/LogIn/");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddShift(EmployeesData shift)
+        {
+            shiftBL.Add(shift);
+            this.UpdateNumberOfActions();
+            return Redirect("Show");
+        }
+
+        private int UpdateNumberOfActions()
+        {
+            int numberOfActions = (int)Session["numOfActions"];
+            numberOfActions = numberOfActions - 1;
+            Session["numOfActions"] = numberOfActions;
+
+            return numberOfActions;
+        }
+    }
+}
---
+++ FactoryManagementFullstack/Models/ShiftBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FactoryManagementFullstack.Models
+{
+    public class ShiftBL
+    {
+        private FactoryManagementDBEntities db = new FactoryManagementDBEntities();
+
+        public List<EmployeesData> GetAllEmployeeShifts()
+        {
+            EmployeeBL employeeBL = new EmployeeBL(); 
+            List<EmployeesData> list = employeeBL.GetAllEmployeesData();
+            List<EmployeesData> orderedList = list.OrderBy(x => x.ShiftID).ToList();
+
+            return orderedList;
+        }
+
+        public void Add(EmployeesData data)
+        {
+            shift shift = new shift();
+            shift.date = data.Date;
+            shift.start_time = data.StartTime;
+            shift.end_time = data.EndTime;
+
+            db.shifts.Add(shift);
+            db.SaveChanges();
+        }
+    }
+}
